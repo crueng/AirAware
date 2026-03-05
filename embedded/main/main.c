9f6b8a84cf56111db61ff1fce2c8a7f40ac7f935
crueng <connor.ruengeling@gmx.de>
2026-03-05 18:42:35 +0100

    Initial Hardware Commit

---
+++ embedded/main/main.c
@@ -0,0 +1,29 @@
+#include "hal/gpio_types.h"
+#include "freertos/FreeRTOS.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <driver/gpio.h>
+
+#define PIN GPIO_NUM_4
+
+void app_main(void)
+{
+	gpio_config_t io = {
+		.pin_bit_mask = 1ULL << PIN,
+		.mode = GPIO_MODE_OUTPUT,
+		.pull_up_en = GPIO_PULLUP_DISABLE,
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,
+		.intr_type = GPIO_INTR_DISABLE,
+	};
+	
+	gpio_config(&io);
+	
+    while (true)
+    {
+		gpio_set_level(PIN, 1);
+		vTaskDelay(pdMS_TO_TICKS(500));
+		gpio_set_level(PIN, 0);
+		vTaskDelay(pdMS_TO_TICKS(500));	
+    }
+}
