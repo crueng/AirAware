0df3c419d68195664611e5697ae96daa9657b2d8
Finja Buecker <finja.buecker@sartorius.com>
2026-03-09 15:09:51 +0100

    feat(AIR-23): add responsive mobile menu and sticky header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,29 +1,44 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  }
     return (
         <header className="top-header">
           <div className="header-left">
+            <button className='menu-btn' aria-label='Menü umschalten' onClick={toggleMenu}>
+              <FontAwesomeIcon icon={isMenuOpen ? faXmark : faBars} size="lg" />
+            </button>
             <h1 className='name'>AirAware</h1>
           </div>
 
-          <nav className="desktop-nav">
+          <nav className={`desktop-nav ${isMenuOpen ? 'mobile-open' : ''}`}>
             <ul>
               <li>
-                <NavLink to="/" end>Dashboard</NavLink>
+                <NavLink to="/" end onClick={closeMenu}>Dashboard</NavLink>
               </li>
               <li>
-                <NavLink to="/temperature" end>Temperatur</NavLink>
+                <NavLink to="/temperature" onClick={closeMenu}>Temperatur</NavLink>
               </li>
               <li>
-                <NavLink to="/humidity" end>Luftfeuchtigkeit</NavLink>
+                <NavLink to="/humidity" onClick={closeMenu}>Luftfeuchtigkeit</NavLink>
               </li>
               <li>
-                <NavLink to="/history">Historie</NavLink>
+                <NavLink to="/history" onClick={closeMenu}>Historie</NavLink>
               </li>
               <li>
-                <NavLink to="/settings">Einstellungen</NavLink>
+                <NavLink to="/settings" onClick={closeMenu}>Einstellungen</NavLink>
               </li>
             </ul>
           </nav>
