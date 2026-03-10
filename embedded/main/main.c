b1747a590405357e5cc3f86aced75ad22890adfe
crueng <connor.ruengeling@gmx.de>
2026-03-10 18:56:16 +0100

    dev: First Wifi Integration

--- embedded/main/main.c
+++ embedded/main/main.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <driver/gpio.h>
+#include "nvs_flash.h"
+
+#include "wifi.h"
 
 #define PIN GPIO_NUM_4
 
@@ -18,7 +21,18 @@ void app_main(void)
 	};
 	
 	gpio_config(&io);
-	
+
+
+	// NVS (Non-Volatile Storage) initialisieren – wird von WiFi benötigt
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	wifi_init_sta();
+
     while (true)
     {
 		gpio_set_level(PIN, 1);
