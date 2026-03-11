b64b83b58f9c34dd21244114bd4cdafeef74814d
Finja Buecker <finja.buecker@sartorius.com>
2026-03-11 08:22:14 +0100

    fix: restore missing backend files from origin/Web-Api

---
+++ backend/Data/AirAwareDbContext.cs
@@ -0,0 +1,28 @@
+using AirAware.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirAware.Data;
+
+public class AirAwareDbContext(DbContextOptions<AirAwareDbContext> options) : DbContext(options)
+{
+    public DbSet<SensorReading> SensorReadings => Set<SensorReading>();
+    public DbSet<AlertThreshold> AlertThresholds => Set<AlertThreshold>();
+    public DbSet<Alert> Alerts => Set<Alert>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // SensorType-Enum als smallint speichern (passend zu pgAdmin-Spalte)
+        modelBuilder.Entity<SensorReading>()
+            .Property(r => r.Type)
+            .HasConversion<short>();
+
+        modelBuilder.Entity<AlertThreshold>()
+            .Property(t => t.Type)
+            .HasConversion<short>();
+
+        // Unique-Constraint auf metric_name (wie in pgAdmin angelegt)
+        modelBuilder.Entity<AlertThreshold>()
+            .HasIndex(t => t.MetricName)
+            .IsUnique();
+    }
+}
