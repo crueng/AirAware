dbbfca945ca707509dd54325f83c22d94e94fd53
Finja Buecker <finja.buecker@sartorius.com>
2026-03-12 18:38:22 +0100

    feat(AIR-27): implement notification and change backend connection

--- frontend/src/apiConfig.tsx
+++ frontend/src/apiConfig.tsx
@@ -1,7 +1,8 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://api.air-aware.de/api';    
 
 export const Endpoints = {
   LatestSensorData: `${BASE_URL}/SensorData/latest`,
   Thresholds: `${BASE_URL}/SensorData/thresholds`,
   Alerts: `${BASE_URL}/SensorData/alerts`,
 };
+
